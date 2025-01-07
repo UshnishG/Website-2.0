20417fa564d653b80b62c5535c80ddb85761c0ee
Ushnish Ghosal <ghosalushnish@gmail.com>
2025-01-07 19:00:35 +0530

    removed mousetrail

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import MouseTrail from "../components/Mousetrail/MouseTrail"; // Correct import path
 import "./globals.css";
 
 const playfairDisplay = Playfair_Display({
@@ -19,7 +18,6 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body className={`${playfairDisplay.variable} antialiased`}>
-        <MouseTrail /> {/* Add the MouseTrail globally */}
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
