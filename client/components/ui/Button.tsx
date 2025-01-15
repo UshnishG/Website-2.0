c141c95e57e128f5dbe4d9b62b9a9759d3a808f9
Ushnish Ghosal <ghosalushnish@gmail.com>
2025-01-15 16:46:25 +0530

    updates

--- client/components/ui/Button.tsx
+++ client/components/ui/Button.tsx
@@ -15,7 +15,7 @@ const DottedButton: React.FC<DottedButtonProps> = ({ src, alt, href, className =
     <img
       src={src}
       alt={alt}
-      className={`rounded-2xl border-2 border-dashed border-black bg-transparent transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none object-contain ${size} ${className}`}
+      className={`rounded-2xl border-2 border-dashed border-white bg-transparent transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none object-contain ${size} ${className}`}
     />
   );
 
